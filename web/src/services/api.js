69cb7bf15e1a2a149ab625db654dded8452bf4a6
Filipe <medeirolp@gmail.com>
2023-01-03 19:01:44 -0300

    refactor: resolve base url

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getItem } from '../utils/storage';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://controle-financeiro-production.up.railway.app',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 });

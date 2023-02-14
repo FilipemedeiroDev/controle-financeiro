3d69723f8e3bae1656eee6f8ca42f58c70cd135d
Filipe <medeirolp@gmail.com>
2023-02-14 15:01:17 -0300

    feat: improving date formatting

--- web/src/utils/dataFormat.js
+++ web/src/utils/dataFormat.js
@@ -4,12 +4,12 @@ export function formatDate(date) {
 
   if (data.getUTCDate() === 1) {
       if (data.getMonth() === 11) {
-          let dataFormatada = (data.getUTCDate()) + "/1/" + (data.getFullYear() + 1);
+          let dataFormatada = (data.getUTCDate() < 10 ? "0" + data.getUTCDate() : data.getUTCDate()) + "/1/" + (data.getFullYear() + 1);
           return dataFormatada;
       }
-      let dataFormatada = ((data.getUTCDate())) + "/" + ((data.getMonth() + 2)) + "/" + data.getFullYear();
+      let dataFormatada = (data.getUTCDate() < 10 ? "0" + data.getUTCDate() : data.getUTCDate()) + "/" + ((data.getMonth() + 2 < 10 ? "0" + 2 : data.getMonth() + 2 )) + "/" + data.getFullYear();
       return dataFormatada
   }
-  let dataFormatada = ((data.getUTCDate())) + "/" + ((data.getMonth() + 1)) + "/" + data.getFullYear();
+  let dataFormatada = ((data.getUTCDate() < 10 ? "0" + data.getUTCDate() : data.getUTCDate()) + "/" + ((data.getMonth() + 1 < 10 ? "0" + (data.getMonth() + 1) : data.getMonth() + 1) )+ "/" + data.getFullYear());
   return dataFormatada
 }
